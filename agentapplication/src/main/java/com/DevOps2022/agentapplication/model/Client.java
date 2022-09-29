5e50517b6f67c96b3c2b0c0784a21fadb9232ce9
senpai132 <acanikolic021@gmail.com>
2022-09-29 13:29:59 +0200

    feat: 6.1. Registration and login

---
+++ agentapplication/src/main/java/com/DevOps2022/agentapplication/model/Client.java
@@ -0,0 +1,18 @@
+package com.DevOps2022.agentapplication.model;
+
+import javax.persistence.CascadeType;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.OneToOne;
+
+//@Entity
+public class Client {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer id;
+    @OneToOne(cascade = CascadeType.ALL)
+    @JoinColumn(name = "user_data_id", referencedColumnName = "id")
+    private User userData;
+}
