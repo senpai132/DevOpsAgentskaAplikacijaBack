92ba00f54056e2dded8eb0c574f2d726542cf443
senpai132 <acanikolic021@gmail.com>
2022-09-29 12:32:12 +0200

    feat: 6.0. Initialized agent application

---
+++ agentapplication/src/main/java/com/DevOps2022/agentapplication/AgentapplicationApplication.java
@@ -0,0 +1,13 @@
+package com.DevOps2022.agentapplication;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AgentapplicationApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AgentapplicationApplication.class, args);
+	}
+
+}
---
+++ agentapplication/src/test/java/com/DevOps2022/agentapplication/AgentapplicationApplicationTests.java
@@ -0,0 +1,13 @@
+package com.DevOps2022.agentapplication;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AgentapplicationApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
